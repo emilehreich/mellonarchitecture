274e28f4793b77147ff7754ecc165f684607d330
Emile Hreich <emile.jd.hreich@gmail.com>
2021-09-27 20:45:43 +0200

    add/sub unit ready for test_bench

--- LAB1/template/vhdl/add_sub.vhd
+++ LAB1/template/vhdl/add_sub.vhd
@@ -14,5 +14,29 @@ entity add_sub is
 end add_sub;
 
 architecture synth of add_sub is
+
+    --signals
+    signal SUB : std_logic_vector(31 downto 0);
+    signal s_result : std_logic_vector(32 downto 0); --33 bits to get the carry out
+    signal s_zero : std_logic;
+    signal s_b : std_logic_vector(31 downto 0);
+
+
 begin
+    
+    operation : process(a, b, sub_mode)
+    begin
+        SUB <= (others => sub_mode);
+        s_b <= SUB xor b;
+
+        s_result <= std_logic_vector(to_unsigned(s_b) + to_unsigned(a) + to_unsigned(sub_mode));
+
+    end process;
+
+    s_zero <= '1' WHEN s_result(31 downto 0) = x"0000" else '0';
+    zero <= s_zero;
+
+    r <= s_result(31 downto 0);
+    carry <= s_result(32);
+
 end synth;
