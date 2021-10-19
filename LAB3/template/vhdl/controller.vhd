801f5e13769204f56a5925aedd552d0af2f77f0d
Emile Hreich <emile.jd.hreich@gmail.com>
2021-10-19 23:26:30 +0200

    Controller continuation for part 4

--- LAB3/template/vhdl/controller.vhd
+++ LAB3/template/vhdl/controller.vhd
@@ -56,7 +56,7 @@ architecture synth of controller is
          s_sel_pc,
          s_branch_op,
          s_pc_add_imm,
-         s_pc_sel_ra,
+         s_pc_sel_a,
          s_pc_sel_imm: std_logic;
 
   signal s_op_alu,
@@ -84,10 +84,18 @@ begin
       when LOAD1 =>
         next_state <= LOAD2;
       when DECODE =>
-        --R_OP
+
         if (("00"&op) = x"3A") then
+          --BREAK
           if (("00"&opx) = x"34") then
             next_state <= BREAK;
+            --CALLR
+          elsif (("00"&opx = x"1D")) then
+            next_state <= CALLR;
+            --JMP
+          elsif (("00"&opx = x"05") or ("00"&opx = x"0D")) then
+            next_state <= JMP;
+            --R_OP
           else
             next_state <= R_OP;
             v_signed <= opx;
@@ -113,7 +121,12 @@ begin
                                  or ("00"&op) = x"1E" or ("00"&op) = x"26"
                                  or ("00"&op) = x"2E" or ("00"&op) = x"36") then
           next_state <= BRANCH;
-
+          --CALL
+        elsif (("00"&op) = x"00") then
+          next_state <= CALL;
+          --JMPI
+        elsif (("00"&op) = x"01") then
+          next_state <= JMPI;
         end if;
       when I_OP =>
         next_state <= FETCH1;
@@ -134,7 +147,7 @@ begin
                     else '0';
   s_read     <= '1' when current_exec_state = FETCH1 or current_exec_state = LOAD1
                     else '0';
-  s_en       <= '1' when current_exec_state = FETCH2
+  s_en       <= '1' when current_exec_state = FETCH2 or current_exec_state = CALLR or current_exec_state = JMP or current_exec_state = JMPI
                     else '0';
   s_ir_en    <= '1' when current_exec_state = FETCH2
                     else '0';
@@ -142,7 +155,7 @@ begin
                     else '0';
   s_sel_rc   <= '1' when current_exec_state = R_OP
                     else '0';
-  s_rf_wren  <= '1' when current_exec_state = LOAD2 or current_exec_state = R_OP or current_exec_state = I_OP or current_exec_state = CALL
+  s_rf_wren  <= '1' when current_exec_state = LOAD2 or current_exec_state = R_OP or current_exec_state = I_OP or current_exec_state = CALL or current_exec_state = CALLR
                     else '0';
   s_sel_addr <= '1' when current_exec_state = LOAD1 or current_exec_state = STORE
                     else '0';
@@ -150,15 +163,15 @@ begin
                     else '0';
   s_signed   <= '1' when current_exec_state = LOAD1 or current_exec_state = STORE or current_exec_state = I_OP
                     else '0';
-  s_sel_pc   <= '0' when current_exec_state = R_OP or current_exec_state = I_OP or current_exec_state = LOAD2 or current_exec_state = CALL
+  s_sel_pc   <= '0' when current_exec_state = R_OP or current_exec_state = I_OP or current_exec_state = LOAD2 or current_exec_state = CALL or current_exec_state = CALLR
                     else '1';
-  s_sel_ra   <= '0' when current_exec_state = R_OP or current_exec_state = I_OP or current_exec_state = LOAD2
+  s_sel_ra   <= '0' when current_exec_state = R_OP or current_exec_state = I_OP or current_exec_state = LOAD2 or current_exec_state = CALLR
                     else '1';
   s_branch_op<= '1' when current_exec_state = BRANCH
                     else '0';
   s_pc_add_imm<= '1'when current_exec_state = BRANCH
                     else '0';
-  s_pc_sel_ra <='1' when current_exec_state = CALL
+  s_pc_sel_a <='1' when current_exec_state = CALL or current_exec_state = CALLR or current_exec_state = JMP or current_exec_state = JMPI
                     else '0';
   --========================================================================
   --op_alu_generation
@@ -172,26 +185,34 @@ begin
       else
         s_op_alu <= opx;
       end if;
+
     elsif ("00"&op = x"0E") then
         -- <= signed
         s_op_alu <= "011001";
+
     elsif ("00"&op = x"16") then
         -- > signed
         s_op_alu <= "011010";
+
     elsif ("00"&op = x"1E") then
         -- not equal
         s_op_alu <= "011011";
+
     elsif ("00"&op = x"26" or "00"&op = x"06") then
         -- equal
         s_op_alu<="011100";
+
     elsif ("00"&op =  x"2E") then
         -- <= unsigned
         s_op_alu<="011101";
+
     elsif ("00"&op = x"36") then
         -- > unsigned
         s_op_alu<="011110";
+
     else
       s_op_alu <= "000000";
+
     end if;
   end process;
 
@@ -213,7 +234,7 @@ begin
     ir_en <= s_ir_en;
     branch_op <= s_branch_op;
     pc_add_imm <= s_pc_add_imm;
-    pc_sel_a   <= s_pc_sel_ra;
+    pc_sel_a   <= s_pc_sel_a;
     pc_sel_imm <= s_pc_sel_imm;
     sel_pc <= s_sel_pc;
     sel_ra <= s_sel_ra;
